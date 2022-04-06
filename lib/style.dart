01df5d11a9c985762393629951431cad840c9af1
Serbun1 <tudor.serban.cismariu@gmail.com>
2022-04-06 10:35:59 +0300

    Changed button to red

--- lib/style.dart
+++ lib/style.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 final ButtonStyle flatButtonStyle = TextButton.styleFrom(
   primary: Colors.white,
-  backgroundColor: Colors.green,
+  backgroundColor: Colors.red,
   minimumSize: Size.fromHeight(40),
   shape: const RoundedRectangleBorder(
     borderRadius: BorderRadius.all(Radius.circular(10.0)),
